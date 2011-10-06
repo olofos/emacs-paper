9e61fad8531b926effdf8482065267f90bd49c8e
olof.ohlsson.sax <olof.ohlsson.sax@gmail.com>
2011-10-06 11:34:03 +0000

    * Added a copyright notices and the license file

--- emacs-paper/emacs-paper.el
+++ emacs-paper/emacs-paper.el
@@ -1,6 +1,20 @@
-;;;; Emacs Paper
-;; 
-;; Emacs package for managing references
+;;; Emacs Paper --- An Emacs mode for managing and browsing references.
+
+;; Copyright 2009-2011 Olof Ohlsson Sax.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, see 
+;; <http://www.gnu.org/licenses/>.
 
 ;;; Set up variables
 (defcustom ep-main-bib-file ""
